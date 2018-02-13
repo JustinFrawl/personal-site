796719efb1066413acaaee6d1c7a454bc132dc60
github username <github email address>
2018-02-13 12:31:39 -0500

    framework coming together

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import Bio from './Bio.js'
+import Bio from './Bio.js';
+import Projects from './Projects.js';
 import './App.css';
-import { Grid, Menu, Segment } from 'semantic-ui-react'
+import { Grid, Menu, Segment, Container, Header } from 'semantic-ui-react'
 import { Link, Switch, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Future Justin Frawley Website</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Container>
+          <Header
+            as='h1'
+            content='Justin Cole Frawley, Fullstack Developer'
+            style={{ fontSize: '4em', fontWeight: 'normal', marginBottom: '1em', marginTop: '1em', color: 'blue' }}
+          />
+        </Container>
 
         <Grid>
         <Segment>
@@ -23,20 +24,15 @@ class App extends Component {
           <Menu pointing secondary size='massive'>
             <Menu.Item as='a'><Link to="/bio">Bio</Link></Menu.Item>
             <Menu.Item as='a'><Link to="/projects">Projects</Link></Menu.Item>
-            <Menu.Item as='a'><Link to="/gitHub">Github</Link></Menu.Item>
+            <Menu.Item as='a'><a href ='https://github.com/JustinFrawl'>Github</a></Menu.Item>
             <Menu.Item as='a'><Link to="/contact">Contact Info</Link></Menu.Item>
           </Menu>
           </Grid.Column>
           </Segment>
-
-        <Grid.Column stretched width={12}>
-          <Segment>
-            This is an stretched grid column. This segment will always match the tab height
-          </Segment>
-        </Grid.Column>
       </Grid>
       <Switch>
       <Route path="/bio" component={Bio} />
+      <Route path="/projects" component={Projects} />
       </Switch>
       </div>
     );
---
+++ src/Projects.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <div>
+      Fuck off, your projects suck dick
+    </div>
+  )
+}
+export default Projects;
