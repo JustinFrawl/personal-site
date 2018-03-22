2140848e706c8468c67dd0fcb5eb87c49b9ccfb8
github username <github email address>
2018-03-21 20:05:19 -0400

    working on personal bio page styling

--- src/Bio.js
+++ src/Bio.js
@@ -4,7 +4,12 @@ const Bio = () => {
 
   return (
     <div>
-    HI!!!!!
+      <h3>
+        Life Before Programming
+      </h3>
+      <p>
+        Prior to attending Flatiron, Justin developed his love for building quality products during years spent working at a craft brewery in a variety of different roles.
+      </p>
     </div>
   )
 }
