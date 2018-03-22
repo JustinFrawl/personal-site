929feb73120c46bae740dd469cfbdcda4aa122f6
github username <github email address>
2018-03-21 21:37:25 -0400

    styling bio page

--- src/Bio.js
+++ src/Bio.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import { Grid, Menu, Segment, Container, Header } from 'semantic-ui-react'
 
 const Bio = () => {
 
   return (
     <div>
+    <Segment>
       <h3>
         Life Before Programming
       </h3>
       <p>
         Prior to attending Flatiron, Justin developed his love for building quality products during years spent working at a craft brewery in a variety of different roles.
       </p>
+    </Segment>
     </div>
   )
 }
