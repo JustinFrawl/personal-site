4621e37c01a5005e96f1687318b835209ee5514a
github username <github email address>
2018-04-04 17:59:06 -0400

    tinkering

--- src/Bio.js
+++ src/Bio.js
@@ -13,7 +13,7 @@ const Bio = () => {
         Prior to attending Flatiron, Justin developed his love for building quality products during years spent working at a craft brewery in a variety of different roles.
       </p>
       <p>
-        At a certain point it became clear that I needed a change of direction.  Having done limited programming in high school and college I took a prep course for Flatiron that led me to really develop a passion for programming.  I believe that at it's very base, programming is pure problem solving in a similar way to the brewing work I was doing before hand.
+
       </p>
     </Segment>
     </div>
